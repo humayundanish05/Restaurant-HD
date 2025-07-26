cdd34403874a5a4d62c70dc2502d39d20e820e7b
humayundanish05 <humayundanish50@gmail.com>
2025-07-26 13:56:17 +0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,10 @@
+﻿// Example: Toggle active filter button
+const filterButtons = document.querySelectorAll(".filters button");
+
+
+filterButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    filterButtons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
